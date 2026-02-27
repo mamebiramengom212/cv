d5dbc1954ee6254bf0ed77036497cae970cd6ebf
mamebiramengom212 <mamebiramengom212@gmail.com>
2026-02-27 15:03:16 +0000

    Add files via upload

---
+++ cv.py
@@ -0,0 +1,32 @@
+import streamlit as st
+
+st.title ("📋 PROFILIO ")
+st.write("""
+Je suis à la recherche d'un projet ambitieux pour pouvoir m'épanouir et développer mes compétences
+""")
+st.subheader("🎓 FORMATIONS")
+st.markdown("""
+* BTS en Geomatique 2026
+* Baccalaureat""")
+
+
+st.subheader("🎯 COMPETENCES")
+st.markdown("""
+* Maitrise de logiciels SIG (QGIS, ArcGIS, etc)
+* Teledetection
+* Levée topographique
+* Extrait de plan cadastrale
+* Modèle Numérique de Terrain
+""")
+
+st.subheader("**Expériences Professionelles** ")
+st.subheader("**JUILLET  - OCTOBRE 2025**")
+st.markdown("""*Stage au impots et domaines dans le bureau du cadastre à Kaolack*""")
+with st.sidebar:
+    st.title("PROFILIO")
+    st.write("*MAME BIRAME NGOM* - Géomaticien")
+    st.markdown("""Fass Casier, Dakar
+    """)
+    st.markdown("""E-MAIL :
+mamebiramengom212@gmail.com 
+     """)
